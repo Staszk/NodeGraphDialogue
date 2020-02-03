83eda025ec72d73aa3111ba919d63dedc886807a
Parker <parker.staszkiewicz@mymail.champlain.edu>
2020-02-03 15:24:39 -0500

    Basic Node implementation

---
+++ Node Graph Dialogue/Assets/Scripts/Editor/Node Graph/Node.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace NodeDialogueSystem
+{
+	public class Node
+	{
+		public Rect rect;
+		public string title;
+
+		public bool isDragged;
+
+		public GUIStyle style;
+
+		public Node(Vector2 position, float width, float height, GUIStyle nodeStyle)
+		{
+			rect = new Rect(position.x, position.y, width, height);
+			style = nodeStyle;
+		}
+
+		public void Drag(Vector2 delta)
+		{
+			rect.position += delta;
+		}
+
+		public void Draw()
+		{
+			GUI.Box(rect, title, style);
+		}
+
+		public bool ProcessEvents(Event e)
+		{
+			switch (e.type)
+			{
+				case EventType.MouseDown:
+					if (e.button == 0)
+					{
+						if (rect.Contains(e.mousePosition))
+						{
+							isDragged = true;
+						}
+
+						GUI.changed = true;
+					}
+					break;
+
+				case EventType.MouseUp:
+					isDragged = false;
+					break;
+
+				case EventType.MouseDrag:
+					if (e.button == 0 && isDragged)
+					{
+						Drag(e.delta);
+						e.Use();
+						return true;
+					}
+					break;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ Node Graph Dialogue/Assets/Scripts/Editor/Node Graph/NodeGraph.cs
@@ -0,0 +1,94 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+namespace NodeDialogueSystem {
+	public class NodeGraph : EditorWindow
+	{
+		private List<Node> nodes;
+
+		private GUIStyle nodeStyle;
+
+		[MenuItem("Window/DialogueGraph")]
+		private static void OpenWindow()
+		{
+			NodeGraph window = GetWindow<NodeGraph>();
+			window.titleContent = new GUIContent("Dialogue Node Graph");
+		}
+
+		private void OnEnable()
+		{
+			nodeStyle = new GUIStyle();
+			nodeStyle.normal.background = EditorGUIUtility.Load("builtin skins/darkskin/images/node1.png") as Texture2D;
+			nodeStyle.border = new RectOffset(12, 12, 12, 12);
+		}
+
+		private void OnGUI()
+		{
+			DrawNodes();
+
+			ProcessNodeEvents(Event.current);
+			ProcessEvents(Event.current);
+
+			if (GUI.changed) Repaint();
+		}
+
+		private void DrawNodes()
+		{
+			if (nodes != null)
+			{
+				for (int i = 0; i < nodes.Count; i++)
+				{
+					nodes[i].Draw();
+				}
+			}
+		}
+
+		private void ProcessNodeEvents(Event e)
+		{
+			if (nodes != null)
+			{
+				for (int i = nodes.Count - 1; i >= 0; --i)
+				{
+					bool guiChanged = nodes[i].ProcessEvents(e);
+
+					if (guiChanged)
+					{
+						GUI.changed = true;
+					}
+				}
+			}
+		}
+
+		private void ProcessEvents(Event e)
+		{
+			switch (e.type)
+			{
+				case EventType.MouseDown:
+					if (e.button == 1)
+					{
+						ProcessContextMenu(e.mousePosition);
+					}
+					break;
+			}
+		}
+
+		private void ProcessContextMenu(Vector2 mousePosition)
+		{
+			GenericMenu gMenu = new GenericMenu();
+			gMenu.AddItem(new GUIContent("Add node"), false, () => OnClickAddNode(mousePosition));
+			gMenu.ShowAsContext();
+		}
+
+		private void OnClickAddNode(Vector2 mousePosition)
+		{
+			if (nodes == null)
+			{
+				nodes = new List<Node>();
+			}
+
+			nodes.Add(new Node(mousePosition, 200, 50, nodeStyle));
+		}
+	}
+}
